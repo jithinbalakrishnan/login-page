c1c535ca156092dd8c3036ac0bfa426cb10a3154
jithinbalakrishnan <jithinbalakrishnan00@gmail.com>
2017-11-21 21:15:38 +0530

    login page UI is updated

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="Login-Wrapper">
+                    <h3 className="login-header-text"><i className="fa fa-user-circle"></i> Login</h3>
+                    <hr/>
+                    <div className="Form-Group">
+                        <label className="label">Username</label>
+                        <input type="text" placeholder="Enter Username" className="input-field" required/>
+                        <label className="label">Password</label>
+                        <input type="password" placeholder="Enter Password" className="input-field" required/>
+                        <hr/>
+                        <button type="submit" className="btn-login"><i className="fa fa-sign-in"></i> Login</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
-
 export default App;
+
+
